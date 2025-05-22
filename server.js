b5e1bee04069a6d9cdd58baa471922e458c4aaf3
ljw8124 <ljw8124@gmail.com>
2025-05-22 20:52:16 +0900

    Mutation Type

--- server.js
+++ server.js
@@ -13,9 +13,16 @@ const typeDefs = gql`
         author: User
     }
     
+    # 서버에서 데이터를 요청할 때 (GET)
     type Query {
         allTweets: [Tweet]
         tweet(id: ID): Tweet
+    }
+    
+    # 사용자가 데이터를 서버에 보내고 싶을 때는 Mutation 을 사용한다 (POST)
+    type Mutation {
+        postTweet(text: String, userId: ID): Tweet
+        deleteTweet(id: ID): Boolean
     }
 `;
 
