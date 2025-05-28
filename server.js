1ff58eea2dd9f0730e32af1f36c8a4f720124b7f
ljw8124 <ljw8124@gmail.com>
2025-05-28 22:00:56 +0900

    주석 추가

--- server.js
+++ server.js
@@ -15,15 +15,17 @@ const typeDefs = gql`
     
     # 서버에서 데이터를 요청할 때 (GET)
     type Query {
-        allTweets: [Tweet]! # ! 의 의미는 nullable 이 아니라는 의미
+        allTweets: [Tweet]! # ! 의 의미는 nullable 이 아니라는 의미, 배열 안에 타입을 넣음으로서 무엇으로 이루어있는지 알려줌
         tweet(id: ID): Tweet
     }
     
     # 사용자가 데이터를 서버에 보내고 싶을 때는 Mutation 을 사용한다 (POST)
+    # 서버에 보내고 DB에 반영
     type Mutation {
         postTweet(text: String, userId: ID): Tweet
         deleteTweet(id: ID): Boolean
     }
+    # 즉 Mutation 을 이용해서 POST, Query 를 이용하여 GET
 `;
 
 const server = new ApolloServer({typeDefs});
